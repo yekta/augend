27e0509670dc3fd8178a50e797dca2dc4d15ec5b
yekta <ngyekta@gmail.com>
2024-11-12 01:05:25 +0300

    not touch hover

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,15 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow not-touch:hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm not-touch:hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm not-touch:hover:bg-accent not-touch:hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground shadow-sm not-touch:hover:bg-secondary/80",
+        ghost:
+          "not-touch:hover:bg-accent not-touch:hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 not-touch:hover:underline",
       },
       size: {
         default: "px-4 py-2",
