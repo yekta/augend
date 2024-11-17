5d9e27fedc1ae58957d83dd9823f0d7770cc3e64
yekta <ngyekta@gmail.com>
2024-11-17 16:18:31 +0300

    Group data fixes

--- src/components/cards/mini-crypto-card.tsx
+++ src/components/cards/mini-crypto-card.tsx
@@ -44,21 +44,22 @@ export default function MiniCryptoCard({
         : ArrowRightIcon;
   return (
     <CardWrapper
+      data-is-loading-error={(isLoadingError && true) || undefined}
+      data-is-pending={(isPending && true) || undefined}
+      data-has-data={(data !== undefined && true) || undefined}
       href={slug ? getCmcUrl(slug) : undefined}
       className={cn("w-1/2 md:w-1/3 xl:w-1/4", className)}
     >
       <div
-        data-is-loading-error={(isLoadingError && true) || undefined}
-        data-is-pending={(isPending && true) || undefined}
         className="w-full flex px-2.5 md:pl-3.5 md:pr-3.75 py-3 md:py-4 gap-2.25 md:gap-3 flex-row border items-center rounded-xl text-left group
-        not-touch:group-hover/card:bg-background-secondary group-active/card:bg-background-secondary relative overflow-hidden"
+        not-touch:group-data-[has-data]/card:group-hover/card:bg-background-secondary group-data-[has-data]/card:group-active/card:bg-background-secondary relative overflow-hidden"
       >
         {isPending ? (
           <div className="size-5 md:size-6 rounded-md bg-foreground animate-skeleton shrink-0" />
         ) : (
           <CryptoIcon
             ticker={ticker}
-            className="size-5 md:size-6 shrink-0 group-data-[is-loading-error]:text-destructive"
+            className="size-5 md:size-6 shrink-0 group-data-[is-loading-error]/card:text-destructive"
           />
         )}
         <div className="flex-1 flex flex-col overflow-hidden gap-1.5 md:gap-1.75">
@@ -66,8 +67,8 @@ export default function MiniCryptoCard({
           <div className="w-full flex flex-row items-center justify-between gap-3">
             <div
               className="shrink min-w-0 flex items-center justify-start gap-1.25 text-muted-foreground text-xs md:text-sm whitespace-nowrap overflow-hidden overflow-ellipsis leading-none md:leading-none
-              group-data-[is-pending]:rounded-sm group-data-[is-pending]:text-transparent group-data-[is-pending]:bg-muted-foreground group-data-[is-pending]:animate-skeleton
-              group-data-[is-loading-error]:text-destructive/60"
+              group-data-[is-pending]/card:rounded-sm group-data-[is-pending]/card:text-transparent group-data-[is-pending]/card:bg-muted-foreground group-data-[is-pending]/card:animate-skeleton
+              group-data-[is-loading-error]/card:text-destructive/60"
             >
               <p className="shrink min-w-0 overflow-hidden overflow-ellipsis">
                 {isPending ? "Load" : ticker ? ticker : "Error"}
@@ -75,7 +76,7 @@ export default function MiniCryptoCard({
               <div
                 data-is-negative={isChangeNegative ? true : undefined}
                 data-is-positive={isChangePositive ? true : undefined}
-                className="flex shrink min-w-0 overflow-hidden overflow-ellipsis items-center justify-start text-muted-foreground group-data-[is-loading-error]:text-destructive data-[is-negative]:text-destructive data-[is-positive]:text-success"
+                className="flex shrink min-w-0 overflow-hidden overflow-ellipsis items-center justify-start text-muted-foreground group-data-[is-loading-error]/card:text-destructive data-[is-negative]/card:text-destructive data-[is-positive]/card:text-success"
               >
                 {data && (
                   <ChangeIcon className="size-3.5 md:size-4 shrink-0 -my-0.5" />
@@ -96,8 +97,8 @@ export default function MiniCryptoCard({
             </div>
             <p
               className="shrink-0 text-muted-foreground text-xs md:text-sm whitespace-nowrap overflow-hidden overflow-ellipsis leading-none md:leading-none
-              group-data-[is-pending]:rounded-sm group-data-[is-pending]:text-transparent group-data-[is-pending]:bg-muted-foreground group-data-[is-pending]:animate-skeleton
-              group-data-[is-loading-error]:text-destructive/60"
+              group-data-[is-pending]/card:rounded-sm group-data-[is-pending]/card:text-transparent group-data-[is-pending]/card:bg-muted-foreground group-data-[is-pending]/card:animate-skeleton
+              group-data-[is-loading-error]/card:text-destructive/60"
             >
               {isPending ? "Load" : rank ? `#${rank}` : "Error"}
             </p>
@@ -106,8 +107,8 @@ export default function MiniCryptoCard({
           <div className="w-full flex flex-row items-center justify-between gap-3">
             <p
               className="shrink text-sm md:text-base font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis leading-none md:leading-none
-              group-data-[is-pending]:rounded-sm group-data-[is-pending]:text-transparent group-data-[is-pending]:bg-foreground group-data-[is-pending]:animate-skeleton
-              group-data-[is-loading-error]:text-destructive"
+              group-data-[is-pending]/card:rounded-sm group-data-[is-pending]/card:text-transparent group-data-[is-pending]/card:bg-foreground group-data-[is-pending]/card:animate-skeleton
+              group-data-[is-loading-error]/card:text-destructive"
             >
               {isPending
                 ? "Loading"
@@ -117,8 +118,8 @@ export default function MiniCryptoCard({
             </p>
             <p
               className="shrink text-sm md:text-base font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis leading-none md:leading-none
-              group-data-[is-pending]:rounded-sm group-data-[is-pending]:text-transparent group-data-[is-pending]:bg-foreground group-data-[is-pending]:animate-skeleton
-              group-data-[is-loading-error]:text-destructive"
+              group-data-[is-pending]/card:rounded-sm group-data-[is-pending]/card:text-transparent group-data-[is-pending]/card:bg-foreground group-data-[is-pending]/card:animate-skeleton
+              group-data-[is-loading-error]/card:text-destructive"
             >
               {isPending
                 ? "Loading"
