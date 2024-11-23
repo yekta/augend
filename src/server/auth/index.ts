c3e33616950ac5dd1ea03301116ddec45f9c2cd5
yekta <ngyekta@gmail.com>
2024-11-23 19:34:39 +0300

    Google provider

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -10,6 +10,7 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth, { DefaultSession } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { cache } from "react";
 
 declare module "next-auth" {
@@ -20,13 +21,30 @@ declare module "next-auth" {
   }
 }
 
+let providers = [];
+if (process.env.AUTH_GOOGLE_ID && process.env.AUTH_GOOGLE_SECRET) {
+  providers.push(Google({ allowDangerousEmailAccountLinking: true }));
+}
+if (process.env.AUTH_GITHUB_ID && process.env.AUTH_GITHUB_SECRET) {
+  providers.push(GitHub({ allowDangerousEmailAccountLinking: true }));
+}
+if (process.env.AUTH_DISCORD_ID && process.env.AUTH_DISCORD_SECRET) {
+  providers.push(Discord({}));
+}
+if (providers.length === 0) {
+  throw new Error(
+    `No OAuth providers configured. Set one of the following pair of environment variables in your env.local file:
+    AUTH_GOOGLE_ID and AUTH_GOOGLE_SECRET, AUTH_GITHUB_ID and AUTH_GITHUB_SECRET, or AUTH_DISCORD_ID and AUTH_DISCORD_SECRET.`
+  );
+}
+
 const {
   auth: uncachedAuth,
   handlers,
   signIn,
   signOut,
 } = NextAuth({
-  providers: [Discord, GitHub],
+  providers,
   adapter: DrizzleAdapter(db, {
     usersTable: usersTable,
     accountsTable: accountsTable,
