00763777c91ca7eacc555360f75a05a08cec3876
yekta <ngyekta@gmail.com>
2024-11-25 19:56:18 +0300

    Sign in page improvements

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,17 +4,20 @@ import { authProviderMap, signIn } from "@/server/auth";
 import { AuthError } from "next-auth";
 
 type Props = {
-  params: Promise<{
-    searchParams?: { callbackUrl: string | undefined };
-  }>;
+  searchParams: Promise<{ callbackUrl?: string; error?: string }>;
 };
 
-export default async function SignInPage({ params }: Props) {
-  const { searchParams } = await params;
+export default async function SignInPage({ searchParams }: Props) {
+  const { callbackUrl, error } = await searchParams;
   return (
     <div className="w-full flex-1 flex flex-col items-center justify-center">
       <div className="w-full flex-1 flex items-center justify-center max-w-7xl">
         <div className="w-full max-w-xs flex flex-col items-center gap-3 pt-12 pb-[calc(3vh+3rem)] px-4 justify-center">
+          {error && (
+            <div className="w-full px-3 text-sm py-2 rounded-lg bg-destructive/10 border border-destructive/15 text-destructive">
+              {getErrorText(error)}
+            </div>
+          )}
           {Object.values(authProviderMap).map((provider) => (
             <form
               key={provider.id}
@@ -23,7 +26,7 @@ export default async function SignInPage({ params }: Props) {
                 "use server";
                 try {
                   await signIn(provider.id, {
-                    redirectTo: searchParams?.callbackUrl ?? "/",
+                    redirectTo: callbackUrl ?? "",
                   });
                 } catch (error) {
                   if (error instanceof AuthError) {
@@ -51,4 +54,16 @@ export default async function SignInPage({ params }: Props) {
       </div>
     </div>
   );
+}
+
+function getErrorText(error: string) {
+  if (error == "OAuthAccountNotLinked") {
+    return (
+      <>
+        This email is already in use. Please sign in with the platform you
+        originally used.
+      </>
+    );
+  }
+  return <>Something went wrong: {error}</>;
 }
