078e35f4e56522888b7c7eed2a2033cd821db114
yekta <ngyekta@gmail.com>
2024-12-07 04:46:45 +0300

    Link to policies

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,5 +1,6 @@
 import ThemeButton from "@/components/theme-button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Footer({ className }: { className?: string }) {
   return (
@@ -10,11 +11,33 @@ export default function Footer({ className }: { className?: string }) {
       )}
     >
       <div className="w-full max-w-7xl px-2 md:px-6">
-        <div className="w-full flex items-center justify-between py-2 gap-4 px-2 ring-1 ring-border rounded-t-xl">
-          <div className="size-8.5" />
-          <p className="text-muted-foreground text-sm font-medium">
-            Augend © 2024
-          </p>
+        <div className="w-full overflow-hidden flex items-center justify-between py-3 md:py-2 gap-2 px-2 ring-1 ring-border rounded-t-xl">
+          <div className="size-8.5 shrink-0 hidden md:flex" />
+          <div className="pl-1.5 md:pl-0 shrink min-0 overflow-hidden font-medium flex flex-col md:flex-row text-sm text-muted-foreground">
+            <p className="shrink min-w-0 text-sm text-left md:text-center">
+              Augend © 2024
+            </p>
+            <span className="px-2 hidden md:block text-muted-more-foreground">
+              •
+            </span>
+            <div className="shrink min-w-0 flex flex-row items-center justify-center">
+              <Link
+                href="/terms"
+                target="_blank"
+                className="shrink text-left md:text-center min-w-0 hover:text-foreground hover:underline"
+              >
+                Terms
+              </Link>
+              <span className="px-2 text-muted-more-foreground">•</span>
+              <Link
+                href="/privacy"
+                target="_blank"
+                className="shrink text-left md:text-center min-w-0 hover:text-foreground hover:underline"
+              >
+                Privacy
+              </Link>
+            </div>
+          </div>
           <ThemeButton />
         </div>
       </div>
