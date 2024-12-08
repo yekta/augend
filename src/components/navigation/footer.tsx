11da04e507975e6b0bd5927e08fe81207959d488
yekta <40706601+yekta@users.noreply.github.com>
2024-12-08 16:47:24 +0300

    Update footer.tsx

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -11,9 +11,9 @@ export default function Footer({ className }: { className?: string }) {
       )}
     >
       <div className="w-full max-w-7xl px-2 md:px-6">
-        <div className="w-full overflow-hidden flex items-center justify-between py-3 md:py-2 gap-2 px-2 ring-1 ring-border rounded-t-xl">
+        <div className="w-full overflow-hidden flex items-center justify-between py-3 md:py-2 gap-2 px-4 ring-1 ring-border rounded-t-xl">
           <div className="size-8.5 shrink-0 hidden md:flex" />
-          <div className="pl-1.5 md:pl-0 pr-1 md:pr-0 shrink min-0 overflow-hidden font-medium flex flex-col md:flex-row text-sm text-muted-foreground">
+          <div className="shrink min-0 overflow-hidden font-medium flex flex-col md:flex-row text-sm text-muted-foreground">
             <p className="shrink min-w-0 text-sm text-left md:text-center">
               Augend © 2024
             </p>
