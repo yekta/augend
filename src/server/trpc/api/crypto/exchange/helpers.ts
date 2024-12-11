be25d5141dc44f91f7551d4c9dca517da8412fff
yekta <ngyekta@gmail.com>
2024-12-11 15:31:23 +0300

    Optional proxy

--- src/server/trpc/api/crypto/exchange/helpers.ts
+++ src/server/trpc/api/crypto/exchange/helpers.ts
@@ -4,18 +4,38 @@ import { TExchange } from "@/server/trpc/api/crypto/exchange/types";
 import ccxt, { Exchange } from "ccxt";
 import { env } from "@/lib/env";
 
+const exchanges: Record<TExchange, Exchange> = {
+  CoinEx: withOptionalProxy(new ccxt.coinex()),
+  Kucoin: withOptionalProxy(new ccxt.kucoin()),
+  Coinbase: withOptionalProxy(new ccxt.coinbaseadvanced()),
+  OKX: withOptionalProxy(new ccxt.okx()),
+  Kraken: withOptionalProxy(new ccxt.kraken()),
+  Binance: withOptionalProxy(
+    new ccxt.binance(
+      env.BINANCE_API_KEY && env.BINANCE_API_SECRET
+        ? {
+            apiKey: env.BINANCE_API_KEY,
+            secret: env.BINANCE_API_SECRET,
+          }
+        : undefined
+    )
+  ),
+};
+
 export function getExchangeInstance(exchange: TExchange): Exchange {
-  if (exchange === "CoinEx") return new ccxt.coinex();
-  if (exchange === "Kucoin") return new ccxt.kucoin();
-  if (exchange === "Coinbase") return new ccxt.coinbaseadvanced();
-  if (exchange === "OKX") return new ccxt.okx();
-  if (exchange === "Kraken") return new ccxt.kraken();
-  return new ccxt.binance(
-    env.BINANCE_API_KEY && env.BINANCE_API_SECRET
-      ? {
-          apiKey: env.BINANCE_API_KEY,
-          secret: env.BINANCE_API_SECRET,
-        }
-      : undefined
-  );
+  if (!exchanges[exchange]) {
+    return exchanges.Binance;
+  }
+  return exchanges[exchange];
+}
+
+function withOptionalProxy(exchange: Exchange) {
+  if (env.CCXT_PROXY_URL) {
+    let url = env.CCXT_PROXY_URL;
+    if (!url.endsWith("/")) {
+      url = url + "/";
+    }
+    exchange.proxyUrl = url;
+  }
+  return exchange;
 }
