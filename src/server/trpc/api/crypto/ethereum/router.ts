95cb2fb5c1c185878b9c2685a08a2cbba910c2ae
yekta <ngyekta@gmail.com>
2024-12-17 13:33:48 +0300

    Remove duplicate folder

--- src/server/trpc/api/crypto/ethereum/router.ts
+++ src/server/trpc/api/crypto/ethereum/router.ts
@@ -1,5 +1,5 @@
 import { ethereumGeneralRouter } from "@/server/trpc/api/crypto/ethereum/general/router";
-import { uniswapRouter } from "@/server/trpc/api/crypto/uniswap/router";
+import { uniswapRouter } from "@/server/trpc/api/crypto/ethereum/uniswap/router";
 import { createTRPCRouter } from "@/server/trpc/setup/trpc";
 
 export const ethereumRouter = createTRPCRouter({
