1cc62cc9400b652b2d6af6a28b9e93de9a7081f0
yekta <ngyekta@gmail.com>
2024-12-19 00:38:50 +0300

    Footer tweaks

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -14,24 +14,27 @@ export default function Footer({ className }: { className?: string }) {
       )}
     >
       <div className="w-full max-w-7xl px-2 md:px-6">
-        <div className="w-full flex items-center justify-between p-3 md:p-2 gap-4 ring-1 ring-border rounded-t-xl">
-          <div className="items-center justify-end gap-2 hidden md:flex">
+        <div className="w-full flex items-center justify-between overflow-hidden p-3 md:p-2 gap-6 ring-1 ring-border rounded-t-xl">
+          <div className="items-center justify-end gap-1.75 hidden md:flex">
             <div className="size-8.5 shrink-0" />
             <div className="size-8.5 shrink-0" />
             <div className="size-8.5 shrink-0" />
           </div>
-          <div className="flex-1 overflow-hidden font-medium justify-center items-start flex flex-col md:flex-row md:items-center md:justify-center text-sm text-muted-foreground">
-            <p className="w-full md:w-auto px-1 shrink min-w-0 text-sm text-left md:text-center">
+          <div
+            className="flex-1 min-w-0 font-medium justify-center items-start flex flex-col md:flex-row md:items-center md:justify-center 
+            text-sm text-muted-foreground"
+          >
+            <p className="w-full md:w-auto px-1 shrink min-w-0 text-sm text-left">
               {siteTitle} © {new Date().getFullYear()}
             </p>
             <span className="px-1 hidden md:block text-muted-more-foreground">
               •
             </span>
-            <div className="w-full md:w-auto shrink overflow-hidden min-w-0 flex flex-row flex-wrap items-center justify-start">
+            <div className="w-full md:w-auto shrink min-w-0 flex flex-row flex-wrap items-center justify-start">
               <Link
                 href="/terms"
                 target="_blank"
-                className="px-1 max-w-full flex items-center text-left md:text-center min-w-0 not-touch:hover:text-foreground not-touch:hover:underline active:underline active:text-foreground rounded relative
+                className="px-1 max-w-full flex items-center text-left min-w-0 not-touch:hover:text-foreground not-touch:hover:underline active:underline active:text-foreground rounded relative
                 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:ring-foreground/50
                 before:w-full before:h-full before:-translate-y-1/2 before:top-1/2 before:-translate-x-1/2 before:left-1/2 before:min-w-[48px] before:min-h-[48px] before:z-[-1] z-0 before:bg-transparent before:absolute"
               >
@@ -41,7 +44,7 @@ export default function Footer({ className }: { className?: string }) {
               <Link
                 href="/privacy"
                 target="_blank"
-                className="px-1 shrink text-left md:text-center min-w-0 not-touch:hover:text-foreground not-touch:hover:underline active:underline active:text-foreground rounded relative
+                className="px-1 shrink text-left min-w-0 not-touch:hover:text-foreground not-touch:hover:underline active:underline active:text-foreground rounded relative
                 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:ring-foreground/50
                 before:w-full before:h-full before:-translate-y-1/2 before:top-1/2 before:-translate-x-1/2 before:left-1/2 before:min-w-[48px] before:min-h-[48px] before:z-[-1] z-0 before:bg-transparent before:absolute"
               >
@@ -49,7 +52,7 @@ export default function Footer({ className }: { className?: string }) {
               </Link>
             </div>
           </div>
-          <div className="flex items-center justify-end gap-2">
+          <div className="flex items-center justify-end gap-1.75">
             <LinkButton
               aria-label={sc.discord.name}
               href={sc.discord.siteHref}
