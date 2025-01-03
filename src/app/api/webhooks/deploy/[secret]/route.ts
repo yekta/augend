ec4b0afba72fcc66329ab7ea651dab7f4c0fa058
yekta <ngyekta@gmail.com>
2025-01-03 06:58:24 +0300

    Try logging static routes

--- src/app/api/webhooks/deploy/[secret]/route.ts
+++ src/app/api/webhooks/deploy/[secret]/route.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+import * as path from "path";
 import { env } from "@/lib/env";
 
 export async function POST(request: Request) {
@@ -10,7 +12,27 @@ export async function POST(request: Request) {
   if (secret !== env.DEPLOY_WEBHOOK_SECRET) {
     return new Response("Invalid secret", { status: 403 });
   }
+
   const body = await request.json();
   console.log("WEBHOOK BODY:", body);
+
+  try {
+    const prerenderManifestPath = path.join(
+      process.cwd(),
+      ".next",
+      "prerender-manifest.json"
+    );
+    const data = fs.readFileSync(prerenderManifestPath, "utf-8");
+    const manifest = JSON.parse(data);
+
+    const relativeRoutes = Object.keys(manifest.routes);
+    const urls = relativeRoutes.map(
+      (route) => `${env.NEXT_PUBLIC_SITE_URL}${route}`
+    );
+    console.log("All fully static URLs:", urls);
+  } catch (error) {
+    console.error("Failed to get static routes:", error);
+    return new Response("Failed to get static routes", { status: 500 });
+  }
   return new Response("ok");
 }
