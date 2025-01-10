61c6ffc74ee696209d9e87e0490c82ab8974fd07
yekta <ngyekta@gmail.com>
2025-01-10 21:02:53 +0300

    Background hover

--- src/app/(doc)/blog/page.tsx
+++ src/app/(doc)/blog/page.tsx
@@ -25,7 +25,7 @@ export default async function Page({}: Props) {
   return (
     <div className="w-full flex flex-col items-center flex-1">
       <div className="w-full flex flex-col justify-center max-w-7xl px-1 md:px-5 pt-4">
-        <h1 className="font-bold text-4xl text-center text-balance px-7 md:px-3">
+        <h1 className="font-bold text-4xl text-center text-balance px-7">
           Blog
         </h1>
         <div className="w-full flex flex-wrap mt-6">
@@ -41,7 +41,7 @@ export default async function Page({}: Props) {
           ) : (
             <div className="w-full flex flex-col items-center text-muted-foreground">
               <PenToolIcon className="size-10" />
-              <p className="w-full text-center px-4 text-balance mt-2">
+              <p className="w-full text-center px-5 text-balance mt-2">
                 No blog posts yet.
               </p>
             </div>
@@ -59,7 +59,7 @@ function PostCard({ post }: { post: PostsOrPages[number] }) {
     <Link
       href={`/blog/${post.slug}`}
       prefetch={false}
-      className="w-full self-stretch flex flex-col border rounded-xl p-2 not-touch:hover:bg-border active:bg-border 
+      className="w-full self-stretch flex flex-col border rounded-xl p-2 not-touch:hover:bg-background-hover active:bg-background-hover 
       focus-visible:ring-1 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-background"
     >
       <div className="aspect-[120/63] flex items-center justify-center bg-border border rounded-md overflow-hidden">
@@ -75,7 +75,7 @@ function PostCard({ post }: { post: PostsOrPages[number] }) {
           <ImageIcon className="size-10 text-muted-foreground" />
         )}
       </div>
-      <div className="px-2 pt-3 pb-1 flex flex-col">
+      <div className="px-2 md:px-3 pt-3 pb-1 md:pt-4 md:pb-2 flex flex-col">
         <h2 className="text-lg font-bold leading-snug">{post.title}</h2>
         {excerpt && <p className="text-muted-foreground mt-1">{excerpt}</p>}
       </div>
